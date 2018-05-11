3caf51d649f56070e5f4a47797273b093703046d
AndyRiome2 <AndyRiome2@hotmail.com>
2018-05-11 14:56:47 +0100

    Add Filter Framework Changes

--- src/components/filters.js
+++ src/components/filters.js
@@ -10,50 +10,50 @@ export default class FilterOptions extends React.Component {
         return (
             <div>
                 <div>
-                    <label id="priority" className="prioirty" text="Priority"/>
+                    <label id="priority" className="prioirty">Priority</label>
                     <select id="prioritySelect" className="prioritySelect">
-                        <option text="Red" value="1"/>
-                        <option text="Amber" value="0.5"/>
-                        <option text="Green" value="0" />
+                        <option value="1">Red</option>
+                        <option value="0.5">Amber</option>
+                        <option value="0">Green</option>
                     </select>
                 </div>
                 <div>
-                    <label id="groups" className="groups" text="Groups"/>
-                    <label id="youngLabel" text="Young"/>
+                    <label id="groups" className="groups">Groups</label>
+                    <label id="youngLabel">Young</label>
                     <input type="checkbox" id="young" className="checkbox" value="young"/>
                 </div>
                 <div>
-                    <label id="middleLabel" text="Middle"/>
+                    <label id="middleLabel">Middle</label>
                     <input type="checkbox" id="Middle" className="checkbox" value="middle"/>
                 </div>
                 <div>
-                    <label id="oldLabel" text="Old"/>
+                    <label id="oldLabel">Old</label>
                     <input type="checkbox" id="Old" className="checkbox" value="old"/>
                 </div>
            
                 <div>
-                    <label id="fields" className="fields" text="Fields"/>
+                    <label id="fields" className="fields">Fields</label>
                         <label id="moneyLabel" text="Money"/>
                         <input type="checkbox" id="money" className="checkbox" value="money"/>
                 </div>
                 <div>
-                    <label id="reportingLabel" text="Reporting"/>
+                    <label id="reportingLabel">Reporting</label>
                     <input type="checkbox" id="reporting" className="checkbox" value="reporting"/>
                 </div>
                 <div>
-                    <label id="locationLabel" text="Money"/>
+                    <label id="locationLabel">Money</label>
                     <input type="checkbox" id="location" className="checkbox" value="location"/>
                 </div>
                 <div>
-                    <label id="callLabel" text="Calls"/>
+                    <label id="callLabel">Calls</label>
                     <input type="checkbox" id="call" className="checkbox" value="call"/>
                 </div>
                 <div>
-                    <label id="websiteHitsLabel" text="Website Hits"/>
+                    <label id="websiteHitsLabel">Website Hits</label>
                     <input type="checkbox" id="websiteHits" className="checkbox" value="hits"/>
                 </div>  
                 <div>
-                    <input type="button" id="search" text="Search" onclick="onSearch_click"/>
+                    <button type="button" onclick="onSearch_click">Search</button>
                 </div>
         </div>
         )
