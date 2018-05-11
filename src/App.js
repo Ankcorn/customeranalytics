9b5628944ede9459fdc33f50796fdca26c1a8b00
ThomasAnkcorn <thomasankcorn@gmail.com>
2018-05-11 12:45:12 +0100

    adding components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Header, Table, Filters, User } from './components' 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          Tom was here
-        </p>
+        <Header className="header">
+          <h1>Customer Analytics</h1>
+          <User />
+        </Header>
+        <div className="content">
+        <Filters className="filters"/>
+        <Table className="table"/>
+        </div>
       </div>
     );
   }
---
+++ src/components/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Header = styled.div`
+    background-color: Aquamarine;
+    height: 60px;
+    padding: 20px;
+    color: white;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-between;
+
+`
+export const User = styled.div`
+    height: 60px;
+    width: 60px;
+    background-image: url("http://en.tintin.com/images/tintin/persos/images/milou.png");
+    border-radius: 50%;
+    background-size: 100% 100%;
+    border: solid 1px #222;
+`
+export const Filters = styled.div`
+    border-radius: 20px;
+    width: 200px;
+    height: 700px;
+    box-shadow: 5px 10px 8px 10px #888888; 
+    background-color: #222
+`
+
+export const Table = styled.div`
+    border-radius: 20px;
+    width: 900px;
+    height: 700px;
+    box-shadow: 5px 10px 8px 10px #888888; 
+    background-color: #222;
+    z-index: 4;
+`
